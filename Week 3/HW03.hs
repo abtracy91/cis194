2d791320443b62292460c19dfac3177f1cff84fa
Alexander Tracy <alex8022@gmail.com>
2015-10-09 19:10:30 -0500

    Finished Homework 3.

--- Week 3/HW03.hs
+++ Week 3/HW03.hs
@@ -1,5 +1,9 @@
 module HW03 where
 
+-- Homework 3
+-- Used https://gist.github.com/adolfopa/2df36cc66dc7ecd2985e
+--  & #haskell-beginners for extensive help
+
 data Expression =
     Var String                   -- Variable
   | Val Int                      -- Integer literal
@@ -34,15 +38,34 @@ type State = String -> Int
 -- Exercise 1 -----------------------------------------
 
 extend :: State -> String -> Int -> State
-extend = undefined
+-- extend: existing State, String -> Int pair to add to State, Result: new state
+-- ex: extend State("a", 1) ("b", 2) => State("a",1, "b",2)
+extend st str1 val str2 = if str1 == str2 then val else st str2
 
 empty :: State
-empty = undefined
+empty _ = 0
 
 -- Exercise 2 -----------------------------------------
 
+boolToInt :: Bool -> Int
+boolToInt b = if b then 1 else 0
+
 evalE :: State -> Expression -> Int
-evalE = undefined
+evalE s (Var v) = s v -- Add var to state
+evalE s (Val n) = n -- Val n is just a number
+evalE s (Op lhs op rhs) =
+    case op of
+        Plus -> x + y
+        Minus -> x - y
+        Times -> x * y
+        Divide -> x `div` y
+        Gt -> boolToInt $ x > y
+        Ge -> boolToInt $ x >= y
+        Lt -> boolToInt $ x < y
+        Le -> boolToInt $ x <= y
+        Eql -> boolToInt $ x == y
+    where x = evalE s lhs
+          y = evalE s rhs
 
 -- Exercise 3 -----------------------------------------
 
@@ -53,17 +76,28 @@ data DietStatement = DAssign String Expression
                    | DSkip
                      deriving (Show, Eq)
 
+-- Example: desugar (Incr "A") == DAssign "A" (Op (var "A") Plus (Val 1))
 desugar :: Statement -> DietStatement
-desugar = undefined
+desugar (Assign v e) = DAssign v e
+desugar (Incr v) = DAssign v (Op (Var v) Plus (Val 1))
+desugar (If e s1 s2) = DIf e (desugar s1) (desugar s2)
+desugar (While e s) = DWhile e (desugar s)
+desugar (For i e th el) = DSequence (desugar i) (DWhile e (DSequence (desugar el) (desugar th)))
+desugar (Sequence x y) = DSequence (desugar x) (desugar y)
+desugar Skip = DSkip
 
 
 -- Exercise 4 -----------------------------------------
 
 evalSimple :: State -> DietStatement -> State
-evalSimple = undefined
+evalSimple s (DAssign v e) = extend s v (evalE s e)
+evalSimple s (DIf e x y) = if (evalE s e) == 1 then evalSimple s x else evalSimple s y
+evalSimple s w@(DWhile e x) = if (evalE s e) == 1 then evalSimple (evalSimple s x) w else s
+evalSimple s (DSequence x y) = evalSimple (evalSimple s x) y
+evalSimple s DSkip = s
 
 run :: State -> Statement -> State
-run = undefined
+run state statement = evalSimple state $ desugar statement
 
 -- Programs -------------------------------------------
 
